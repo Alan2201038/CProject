a3dd215792785c2c78b4967491e5059095b6a732
Alan2201038 <2201038@sit.singaporetech.edu.sg>
2022-10-25 22:51:25 +0800

    feat: Added a Hi.c program to test!

---
+++ Hi.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+void main(){
+    printf("Hello World! Send Nudes");
+}
+
+// Use this to compile the code and create a .exe file: gcc Hi.c -o [Name of .exe file]
+// Use this to run the compiled code (AKA the exe file): .\[Name of file].exe
