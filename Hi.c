f42548b91a52eecc02bf51a6a6f0469eff9cdf05
Alan2201038 <2201038@sit.singaporetech.edu.sg>
2022-11-19 21:44:37 +0800

    feat: Deleted test files.

--- Hi.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-void main(){
-    printf("Hello World! Send Nudes");
-}
-
-// Use this to compile the code and create a .exe file: gcc Hi.c -o [Name of .exe file]
-// Use this to run the compiled code (AKA the exe file): .\[Name of file].exe
